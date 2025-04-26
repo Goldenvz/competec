9d4f4528dd86793bb210af33f82b89ae458d7a26
Goldenvz <guilhermelopesdourado@gmail.com>
2025-04-26 11:22:57 -0300

    Aula 1, Opala e Saveiro

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
+	    
+	    char nome = 'G';
+	    int idade = 15;
+	    boolean cafedamanha = true;
+	    
+	    int valorLaranja = 8;
+	    int valorUva = 16;
+	    int UvaELaranja = valorLaranja += valorUva;
+	    System.out.println(UvaELaranja);
+	    
+	   Scanner entrada = new Scanner(System.in);
+	   
+	   System.out.print("Digite o valor da Savero: ");
+	   int Savero = entrada.nextInt();
+	   
+	   System.out.print("Digite o valor da Opala: ");
+	   int Opala = entrada.nextInt();
+	   
+	   System.out.println("Resultado da soma = "+ (Opala + Savero));
+	
+	
+	    float notaAlunos = 0.6f;
+	    System.out.println("São Paulo foi campeão três vezes mundial!!");
 		System.out.println("Hello World");
 	}
 }
