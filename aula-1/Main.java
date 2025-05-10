2254a499d113ea53f9dbca6330e02022b208e09e
Goldenvz <guilhermelopesdourado@gmail.com>
2025-05-10 11:51:35 -0300

    Update Main.java

--- aula-1/Main.java
+++ aula-1/Main.java
@@ -1,7 +1,39 @@
+
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+	    
+	   
+	  boolean nomeGKS = true; 
+	    boolean nomeFerpes = true;
+	   boolean ehVerdade = nomeGKS != nomeFerpes;
+	  System.out.println(nomeGKS!=nomeFerpes );
+	  
+	  int idadeGks = 15;
+	  int idadeItu = 23;
+	 
+	  System.out.println(idadeGks<idadeItu);
+	  
+	  boolean estaRelampago = false;
+	 boolean estaChuva = false;
+	 
+	 
+	  if (estaRelampago&&estaChuva) {
+	      System.out.println("eu não saio de casa");
+	  }
+	       
+	       else   { System.out.println ("eu saio de casa");
+	       
+	       }
+	       
+	       boolean macarrao = false;
+	       boolean strogonoff = false;
+	       
+	       if (macarrao||strogonoff) { System.out.println ("fico muito feliz");
+	       }
+	       else { System.out.println ("fico triste");
+	       
+	       }
+	      }
 	}
-}
+
