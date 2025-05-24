4dbad90a08340b583c8822910c96ec9962451a00
Goldenvz <guilhermelopesdourado@gmail.com>
2025-05-24 11:30:48 -0300

    Update Main.java

--- aula-3/Main.java
+++ aula-3/Main.java
@@ -1,7 +1,38 @@
+import java.util.Scanner;
+
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
-	}
+	  
+	   /*     Scanner entrada = new Scanner(System.in);
+	        
+	        int g = 0;
+	        String palavra = entrada.nextLine();
+	        while (g <5){
+	            System.out.println (palavra  ); 
+	            g++;}
+	           
+	            Scanner entrada = new Scanner(System.in);
+	            
+	            int x = 1;
+	            int soma = 0;
+	            while (x <=100) {
+	               
+	            soma+= x;
+	            x++;
+	            }System.out.println(soma);*/
+	     
+	   /*  Scanner entrada = new Scanner(System.in);
+             // tabuada * 1
+             // tabuada * 2
+             // tabuada * 3
+             // ...
+             // tabuada * 10
+             int tabuada = entrada.nextInt();
+             for (int n = 1; n <=10; n++){
+                 System.out.println(n*tabuada);
+             }*/
+
+    
 }
+	}
